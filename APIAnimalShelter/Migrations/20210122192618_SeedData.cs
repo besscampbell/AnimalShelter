505e941e48d61c0a232ccebd0f31ce07c1f6733f
Bess Campbell <bess.k.campbell@gmail.com>
2021-01-22 11:29:05 -0800

    Seed database

---
+++ APIAnimalShelter/Migrations/20210122192618_SeedData.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIAnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cats",
+                columns: new[] { "CatId", "Age", "Breed", "Coloring", "DateAvailable", "Name", "Personality", "Sex" },
+                values: new object[,]
+                {
+                    { 1, 3, "DSH", "Tuxedo", new DateTime(2021, 1, 22, 11, 26, 17, 714, DateTimeKind.Local).AddTicks(6710), "Boots", "Boots is very curious. He plays well with cats and dogs but does not do well around children.", "Male" },
+                    { 2, 0, "Bombay", "Black", new DateTime(2021, 1, 22, 11, 26, 17, 744, DateTimeKind.Local).AddTicks(3640), "Snowball", "Snowball is 6 months old and ready for a playmate. She is very funny and acrobatic.", "Female" },
+                    { 3, 12, "Persian", "Grey", new DateTime(2021, 1, 22, 11, 26, 17, 744, DateTimeKind.Local).AddTicks(3660), "Agatha", "Agatha is a senior cat looking for her furrever lap. She loves snuggles and head pets", "Female" },
+                    { 4, 4, "Siamese/DSH", "Flame Point", new DateTime(2021, 1, 22, 11, 26, 17, 744, DateTimeKind.Local).AddTicks(3670), "Robert", "Robert loves playing tag but can get a little rowdy in his play. Might not do well around children.", "Male" },
+                    { 5, 7, "Scottish Fold", "Silver", new DateTime(2021, 1, 22, 11, 26, 17, 744, DateTimeKind.Local).AddTicks(3670), "Squish", "Squish is the best. You all are crazy if you don't try to adopt this cat right now.", "Male" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "Age", "Breed", "Coloring", "DateAvailable", "Name", "Personality", "Sex" },
+                values: new object[,]
+                {
+                    { 1, 6, "American Pit Bull", "Blue grey, white", new DateTime(2021, 1, 22, 11, 26, 17, 745, DateTimeKind.Local).AddTicks(3080), "Jackson", "Jackson gets easily frustrated and can exhibit this in a variety of ways. He will need positive outlets for his energy and a calm environment.", "Male" },
+                    { 2, 2, "Mastiff", "Tan, black", new DateTime(2021, 1, 22, 11, 26, 17, 745, DateTimeKind.Local).AddTicks(4190), "Chickie", "Chickie will flourish in a calm environment and a low-traffic home. She can be worried about new people, new surroundings and touch. She prefers to take things at her own pace.", "Female" },
+                    { 3, 3, "Chihuahua/Dachshund", "TriColor", new DateTime(2021, 1, 22, 11, 26, 17, 745, DateTimeKind.Local).AddTicks(4210), "Miss Precious", "Miss Precious has a history of biting a person or another animal. She would do best in an adult only home", "Female" },
+                    { 4, 2, "Rottweiler", "Black, Tan", new DateTime(2021, 1, 22, 11, 26, 17, 745, DateTimeKind.Local).AddTicks(4220), "Zeus", "Zeus had a growth mindset and loves to learn. He needs positive outlets for his energy and a calm environment.", "Male" },
+                    { 5, 10, "Pointer/American Pit Bull", "White, Tan", new DateTime(2021, 1, 22, 11, 26, 17, 745, DateTimeKind.Local).AddTicks(4220), "Uma", "Although Uma is no longer a puppy, she is still active and enjoys going for daily walks or runs. When she's not out and about, you can find her napping on a dog bed or a couch.", "Female" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 5);
+        }
+    }
+}
--- APIAnimalShelter/Models/APIAnimalShelterContext.cs
+++ APIAnimalShelter/Models/APIAnimalShelterContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace APIAnimalShelter.Models
 {
@@ -11,5 +12,25 @@ namespace APIAnimalShelter.Models
 
     public DbSet<Dog> Dogs { get; set; }
     public DbSet<Cat> Cats { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Cat>()
+        .HasData(
+          new Cat { CatId = 1, Name = "Boots", Age = 3, Sex = "Male", Breed = "DSH", Coloring = "Tuxedo", DateAvailable = DateTime.Now, Personality = "Boots is very curious. He plays well with cats and dogs but does not do well around children." },
+          new Cat { CatId = 2, Name = "Snowball", Age = 0, Sex = "Female", Breed = "Bombay", Coloring = "Black", DateAvailable = DateTime.Now, Personality = "Snowball is 6 months old and ready for a playmate. She is very funny and acrobatic." },
+          new Cat { CatId = 3, Name = "Agatha", Age = 12, Sex = "Female", Breed = "Persian", Coloring = "Grey", DateAvailable = DateTime.Now, Personality = "Agatha is a senior cat looking for her furrever lap. She loves snuggles and head pets" },
+          new Cat { CatId = 4, Name = "Robert", Age = 4, Sex = "Male", Breed = "Siamese/DSH", Coloring = "Flame Point", DateAvailable = DateTime.Now, Personality = "Robert loves playing tag but can get a little rowdy in his play. Might not do well around children." },
+          new Cat { CatId = 5, Name = "Squish", Age = 7, Sex = "Male", Breed = "Scottish Fold", Coloring = "Silver", DateAvailable = DateTime.Now, Personality = "Squish is the best. You all are crazy if you don't try to adopt this cat right now." }
+        );
+      builder.Entity<Dog>()
+        .HasData(
+          new Dog { DogId = 1, Name = "Jackson", Age = 6, Sex = "Male", Breed = "American Pit Bull", Coloring = "Blue grey, white", DateAvailable = DateTime.Now, Personality = "Jackson gets easily frustrated and can exhibit this in a variety of ways. He will need positive outlets for his energy and a calm environment."},
+          new Dog { DogId = 2, Name = "Chickie", Age = 2, Sex = "Female", Breed = "Mastiff", Coloring = "Tan, black", DateAvailable = DateTime.Now, Personality = "Chickie will flourish in a calm environment and a low-traffic home. She can be worried about new people, new surroundings and touch. She prefers to take things at her own pace."},
+          new Dog { DogId = 3, Name = "Miss Precious", Age = 3, Sex = "Female", Breed = "Chihuahua/Dachshund", Coloring = "TriColor", DateAvailable = DateTime.Now, Personality = "Miss Precious has a history of biting a person or another animal. She would do best in an adult only home"},
+          new Dog { DogId = 4, Name = "Zeus", Age = 2, Sex = "Male", Breed = "Rottweiler", Coloring = "Black, Tan", DateAvailable = DateTime.Now, Personality = "Zeus had a growth mindset and loves to learn. He needs positive outlets for his energy and a calm environment."},
+          new Dog { DogId = 5, Name = "Uma", Age = 10, Sex = "Female", Breed = "Pointer/American Pit Bull", Coloring = "White, Tan", DateAvailable = DateTime.Now, Personality = "Although Uma is no longer a puppy, she is still active and enjoys going for daily walks or runs. When she's not out and about, you can find her napping on a dog bed or a couch."}
+        ); 
+    }
   }
 }
